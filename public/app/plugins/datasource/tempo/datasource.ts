9b27b3cc742c10685c630091c60da5a158711739
Fabrizio <fabrizio.casati@grafana.com>
2023-08-14 17:25:56 +0200

    Fix usage of isFeatureAvailable

--- public/app/plugins/datasource/tempo/datasource.ts
+++ public/app/plugins/datasource/tempo/datasource.ts
@@ -318,7 +318,7 @@ export class TempoDatasource extends DataSourceWithBackend<TempoQuery, TempoJson
           streaming: config.featureToggles.traceQLStreaming,
         });
 
-        if (config.featureToggles.traceQLStreaming) {
+        if (config.featureToggles.traceQLStreaming && this.isFeatureAvailable(FeatureName.streaming)) {
           subQueries.push(this.handleStreamingSearch(options, targets.traceqlSearch, queryValue));
         } else {
           subQueries.push(
